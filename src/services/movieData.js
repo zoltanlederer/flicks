28aca791d83025e0fb3314fec2793fa32003ce14
Zoltan Lederer <zoltanlederer@yahoo.com>
2022-01-19 14:05:53 +0100

    Initial  commit

--- src/services/movieData.js
+++ src/services/movieData.js
@@ -1,9 +1,5 @@
 import axios from 'axios'
 
-// https://cors-anywhere.herokuapp.com/
-
-// https://api.themoviedb.org/3/movie/550?api_key=ba0c945c141b8fb9b78869c1c9811e6b
-
 const baseUrl = 'https://api.themoviedb.org/3';
 const key = process.env.REACT_APP_KEY
 
