6375249bdc32d81fac3dd615e533aa77ec2abb9a
Zoltan Lederer <zoltanlederer@yahoo.com>
2022-02-17 11:07:13 +0100

    key

--- src/services/movieData.js
+++ src/services/movieData.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const baseUrl = 'https://api.themoviedb.org/3';
-const key = process.env.REACT_APP_KEY
+const key = '?api_key=ba0c945c141b8fb9b78869c1c9811e6b'
 
 const get = (urlSecondPart, query = '', lang = 'en') => {
   return axios.get(`${baseUrl}/${urlSecondPart}${key}${query}${lang}`).then(res => res.data)
